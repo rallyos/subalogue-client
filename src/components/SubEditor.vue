e7605099c09af1425e5449969ccf8f149870ea81
Dimitar Ralev <dralev@shiftingphotons.dev>
2022-07-02 23:34:15 +0300

    Improve sub editor and...apply prettier

--- src/components/SubEditor.vue
+++ src/components/SubEditor.vue
@@ -1,115 +1,143 @@
 <template>
   <form action="">
     <b-field grouped>
-      <b-field expanded>
-          <b-input
-              v-model="sub.name"
-              placeholder="Name"
-              icon-pack="fal"
-              required>
-          </b-input>
-      </b-field>
+      <div class="column is-2">
+        <b-input v-model="sub.name" placeholder="Name" icon-pack="fal" required>
+        </b-input>
+      </div>
 
-      <b-field>
-          <b-input
-              type="url"
-              v-model="sub.url"
-              placeholder="Url"
-              icon-pack="fal"
-              required>
-          </b-input>
-      </b-field>
+      <div class="column is-2">
+        <b-input
+          type="url"
+          v-model="sub.url"
+          placeholder="Url"
+          icon-pack="fal"
+          required
+        >
+        </b-input>
+      </div>
 
-      <b-field>
-          <b-input
-              type="number"
-              step="0.01"
-              min="0"
-              v-model="sub.price"
-              icon-pack="fal"
-              placeholder="Price"
-              required>
-          </b-input>
-      </b-field>
+      <div class="column is-2">
+        <b-input
+          type="number"
+          step="0.01"
+          min="0"
+          v-model="sub.price"
+          icon-pack="fal"
+          placeholder="Price"
+          required
+        >
+        </b-input>
+      </div>
 
-      <b-field>
-          <b-select placeholder="Recurring" v-model="sub.recurring" icon-pack="fal" required>
-              <option
-                  v-for="option in recurringOptions"
-                  :value="option"
-                  :key="option">
-                  {{ option }}
-              </option>
-          </b-select>
-      </b-field>
-
-      <b-field>
-          <b-datepicker
-              placeholder="Billing Date"
-              icon="calendar-alt"
-              icon-pack="fal"
-              v-model="sub.billing_date"
-              required>
-          </b-datepicker>
-      </b-field>
-
-      <b-field>
-          <b-button outlined
-            type="is-success"
+      <div class="column is-3">
+        <b-field grouped>
+          <b-select
+            placeholder="Recurring"
+            v-model="sub.recurring"
             icon-pack="fal"
-            icon-left="layer-plus"
-            :disabled="invalidForm"
-            @click="triggerAction">
-              {{sub.id ? 'Update' : 'Add'}}
-            </b-button>
-      </b-field>
+            style="width: 38%;"
+            required
+          >
+            <option
+              v-for="option in recurringOptions"
+              :value="option"
+              :key="option"
+            >
+              {{ option }}
+            </option>
+          </b-select>
 
-      <b-field>
-          <b-button type="is-danger" outlined @click="$parent.inlineSubForm = false">Close</b-button>
-      </b-field>
+          <b-datepicker
+            placeholder="Date"
+            icon="calendar-alt"
+            icon-pack="fal"
+            v-model="sub.billing_date"
+            style="width: 38%;"
+            required
+          >
+          </b-datepicker>
+        </b-field>
+      </div>
+
+      <div class="column is-3">
+        <b-button
+          outlined
+          type="is-success"
+          icon-pack="fal"
+          icon-left="layer-plus"
+          :disabled="invalidForm"
+          @click="triggerAction"
+          class="mr-4"
+        >
+          {{ sub.id ? "Update" : "Add" }}
+        </b-button>
+
+        <b-button
+          type="is-danger"
+          outlined
+          @click="$parent.inlineSubForm = false"
+          >Close</b-button
+        >
+      </div>
     </b-field>
   </form>
 </template>
 
 <script>
-import currency from 'currency.js'
+import currency from "currency.js";
 
 export default {
   name: "SubEditor",
-  data: () => ({recurringOptions: ['monthly', 'yearly']}),
-  props: ['sub'],
+  data: () => ({ recurringOptions: ["monthly", "yearly"] }),
+  props: ["sub"],
   computed: {
-    invalidForm: function() { return !this.sub.name && !this.sub.price && !this.sub.url && !this.sub.recurring && !this.sub.billing_date }
+    invalidForm: function() {
+      return (
+        !this.sub.name &&
+        !this.sub.price &&
+        !this.sub.url &&
+        !this.sub.recurring &&
+        !this.sub.billing_date
+      );
+    },
   },
   methods: {
     // Return YYYY-MM-DDT00:00:00Z format
     buildSafeDateString: function(date) {
-      return date.getFullYear() + '-' + ("0" + date.getMonth()).slice(-2) + '-' + ("0" + date.getDate()).slice(-2) + 'T00:00:00Z'
+      return (
+        date.getFullYear() +
+        "-" +
+        ("0" + date.getMonth()).slice(-2) +
+        "-" +
+        ("0" + date.getDate()).slice(-2) +
+        "T00:00:00Z"
+      );
     },
     triggerAction: function() {
       this.$parent.inlineSubForm = false;
-      this.sub.price = currency(this.sub.price).intValue
+      this.sub.price = currency(this.sub.price).intValue;
 
       if (!this.sub.id) {
-        this.$store.dispatch('createSubscription', {
+        this.$store.dispatch("createSubscription", {
           id: this.sub.id,
           name: this.sub.name,
           url: this.sub.url,
           price: this.sub.price,
           recurring: this.sub.recurring,
-          billing_date: this.buildSafeDateString(this.sub.billing_date)
-        })
+          billing_date: this.buildSafeDateString(this.sub.billing_date),
+        });
       } else {
-        this.$store.dispatch('updateSubscription', {
+        this.$store.dispatch("updateSubscription", {
           id: this.sub.id,
           name: this.sub.name,
           url: this.sub.url,
           price: this.sub.price,
           recurring: this.sub.recurring,
-          billing_date: this.buildSafeDateString(this.sub.billing_date)
-        })
+          billing_date: this.buildSafeDateString(this.sub.billing_date),
+        });
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
